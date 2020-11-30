88fea010853d21dd8b091fa6ae75e571c3a25aa6
Dom Batten <dominic.batten@googlemail.com>
2020-11-30 10:21:56 +0000

    Add zsh custom keybindings

---
+++ zsh/custom/keybind.zsh
@@ -0,0 +1,12 @@
+bindkey -v
+
+bindkey '^e' end-of-line
+bindkey '^a' beginning-of-line
+bindkey '^b' backward-word
+bindkey '^w' forward-word
+bindkey '^h' backward-char
+bindkey '^l' forward-char
+bindkey '^x' delete-char
+bindkey '^d' kill-word
+bindkey '^k' vi-kill-eol
+bindkey '^u' kill-whole-line
