cdf865a7c92387515dcc38cd6860de1a049c4584
Dom Batten <dominic.batten@googlemail.com>
2020-12-01 17:39:44 +0000

    Add utility functions
    - fzf aliases and print to command line with shortcut CTRL-F CTRL-A
    - fzf recent commits to fixup

--- zsh/custom/function.zsh
+++ zsh/custom/function.zsh
@@ -1,9 +1,41 @@
+# GIT {{{1
 function fixup() {
   local current_sha="$(git rev-parse HEAD)"
 
   git commit --fixup $current_sha
 }
 
+function rhead() {
+  git rebase -i HEAD~$1
+}
+
+function git_fix() {
+  git log -n 20 --oneline | fzf | cut -d' ' -f 1 | xargs git commit --no-verify --fixup
+}
+
+# DOCKER {{{1
 function dexec() {
   docker exec -it $1 /bin/bash
 }
+
+# FZF {{{1
+# ALIASES {{{2
+# fuzzy find an alias and paste to command-line
+function fza() {
+  local sel="$(alias | fzf | cut -d= -f1)"
+  [ -n "$sel" ] && print -z -- ${sel}
+}
+
+# fza but as a widget to be used with a key binding
+function _fuzzy-alias() {
+  local sel="$(alias | fzf | cut -d= -f1)"
+  if [ -n "$sel" ]; then
+    LBUFFER="$sel"
+    RBUFFER=''
+  fi
+  [ -n "$widgets[autosuggest-clear]" ] && zle autosuggest-clear
+  zle reset-prompt
+}
+
+zle -N _fuzzy-alias
+bindkey -M viins '^f^a' _fuzzy-alias
