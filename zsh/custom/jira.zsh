554dd61f9af7e3ac5b2d697d3e316ea7ed55b8fd
Dom Batten <dominic.batten@googlemail.com>
2020-12-08 16:53:20 +0000

    Fix jira function to work with zsh

--- zsh/custom/jira.zsh
+++ zsh/custom/jira.zsh
@@ -1,12 +1,12 @@
 # start a new jira ticket
 function jira() {
   if [[ -n $(git status -s) ]]; then
-    echo "You have unstaged changes! Aborting" && exit 1
+    echo "You have unstaged changes! Aborting" && return 0
   fi
 
-  read -p 'Please provide the Jira issue url: ' issue_url
+  read "issue_url?Please provide the Jira issue url: "
 
-  [ -z "$issue_url" ] && echo "Please enter a url!" && exit 1
+  [ -z "$issue_url" ] && echo "Please enter a url!" && return 0
 
   issue_id=${issue_url##*/}
 
@@ -18,15 +18,15 @@ function jira() {
 
   error=$(jq -r '.errorMessages[0]' <<< ${body})
 
-  [ "$error" != "null" ] && echo "Error: ${error}" && exit 1
+  [ "$error" != "null" ] && echo "Error: ${error}" && return 0
 
   default_ticket_summary=$(jq -r '.fields.summary' <<< ${body} |
     sed -e 's/[()\.:]//g' -e 's/[[:blank:]]/-/g'  |
     awk '{print tolower($1);}'
   )
-  ticket_type=$(jq -r '.fields.issuetype.name' <<< ${body})
+  ticket_type=${$(jq -r '.fields.issuetype.name' <<< ${body}):l}
 
-  read -p "Enter branch name [${default_ticket_summary}]: " branch_description
+  read "branch_description?Enter branch name [${default_ticket_summary}]: "
 
   if [[ -z "$branch_description" ]]; then
     branch_description="$default_ticket_summary"
