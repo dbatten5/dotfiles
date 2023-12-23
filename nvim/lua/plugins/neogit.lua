5435d31abdbe5860d61233d44352bd094f976533
Dom Batten <dominic.batten@googlemail.com>
2023-12-23 13:08:32 +0000

    more additions

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,20 @@
+return {
+  "NeogitOrg/neogit",
+  cmd = "Neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = true,
+  opts = {
+    integrations = {
+      telescope = true,
+      diffview = true,
+    },
+    kind = "split",
+  },
+  keys = {
+    { "<space>g", "<cmd>Neogit<cr>", desc = "Open Neogit" },
+  },
+}
---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = [[<c-t>]],
+    direction = "vertical",
+  },
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+    local map = vim.keymap.set
+    map("t", "<ESC>", "<c-\\><c-n>", { noremap = true })
+  end,
+}
