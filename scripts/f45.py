4a11bb4443644de3fcd2b20b5da60c41a9158aab
Dom Batten <dominic.batten@googlemail.com>
2025-06-06 09:53:30 +0100

    Improve f45 script

--- scripts/f45.py
+++ scripts/f45.py
@@ -1,45 +1,102 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+# "typer",
+# "attrs",
+# ]
+# ///
+
+import subprocess
+import rich
 import random
-import sys
+import glob
+import pathlib
+from typing_extensions import Annotated
+import typer
+import enum
+import attrs
+from collections.abc import Iterable
 
-STRENGTH = {
-    "All Star": 2,
-    "Angry Bird": 1,
-    "GOAT": 1,
-    "MKatz": 3,
-    "Pegasus": 1,
-    "Piston": 4,
+
+class WorkoutName(enum.StrEnum):
+    ABACUS = "Abacus"
+    ALL_STAR = "All Star"
+    ANGRY_BIRD = "Angry Bird"
+    EMPIRE = "Empire"
+    GOAT = "Goat"
+    MKATZ = "Mkatz"
+    MONT_BLANC = "Mont Blanc"
+    MVP = "MVP"
+    PEGASUS = "Pegasus"
+    PISTON = "Piston"
+    QUARTERBACKS = "Quarterbacks"
+    TWENTY_TWO = "22"
+
+
+class WorkoutType(enum.StrEnum):
+    CARDIO = "cardio"
+    STRENGTH = "strength"
+    ALL = "all"
+
+
+@attrs.frozen
+class Video:
+    name: WorkoutName
+    ordinal: int
+    path: pathlib.Path
+    type: WorkoutType
+
+
+VIDEO_MAPPING: dict[str, WorkoutType] = {
+    WorkoutName.ALL_STAR: WorkoutType.STRENGTH,
+    WorkoutName.ANGRY_BIRD: WorkoutType.STRENGTH,
+    WorkoutName.GOAT: WorkoutType.STRENGTH,
+    WorkoutName.MKATZ: WorkoutType.STRENGTH,
+    WorkoutName.PEGASUS: WorkoutType.STRENGTH,
+    WorkoutName.PISTON: WorkoutType.STRENGTH,
+    WorkoutName.TWENTY_TWO: WorkoutType.CARDIO,
+    WorkoutName.ABACUS: WorkoutType.CARDIO,
+    WorkoutName.EMPIRE: WorkoutType.CARDIO,
+    WorkoutName.MONT_BLANC: WorkoutType.CARDIO,
+    WorkoutName.MVP: WorkoutType.CARDIO,
+    WorkoutName.QUARTERBACKS: WorkoutType.CARDIO,
 }
 
-CARDIO = {
-    22: 2,
-    "Abacus": 3,
-    "Empire": 2,
-    "Mont Blanc": 1,
-    "MVP": 4,
-    "Quarterbacks": 1,
-}
+app = typer.Typer()
 
 
-def pick_random_vid(videos: dict) -> tuple[str, int]:
-    key = random.choice(list(videos.keys()))
-    idx = random.randint(0, videos[key] - 1)
-    return key, idx
+def collect_videos(by_type: WorkoutType) -> Iterable[Video]:
+    workouts: dict[WorkoutName, list[Video]] = {}
+    workout_names = [w for w in WorkoutName]
+    for path in glob.glob("/Users/dom.batten/Documents/Workouts/*/**"):
+        for workout_name in workout_names:
+            if workout_name in path:
+                video_type = VIDEO_MAPPING.get(workout_name)
+                video = Video(
+                    name=workout_name,
+                    ordinal=len(workouts.get(workout_name, [])) + 1,
+                    path=pathlib.Path(path),
+                    type=video_type,
+                )
+                workouts.setdefault(workout_name, []).append(video)
+
+    all_videos = [video for videos in workouts.values() for video in videos]
+
+    if by_type == WorkoutType.ALL:
+        yield from all_videos
+    else:
+        yield from (video for video in all_videos if video.type == by_type)
 
 
-args = sys.argv
+@app.command()
+def hello(type: Annotated[WorkoutType, typer.Argument()] = WorkoutType.ALL):
+    workouts = list(collect_videos(type))
+    random_video = random.choice(workouts)
+    rich.print(f"{random_video.name} video {random_video.ordinal}")
+    if typer.confirm("Let's go?", abort=True):
+        subprocess.run(["open", random_video.path])
 
-if len(args) == 2:
-    arg = args[1]
 
-    if arg == "cardio":
-        video, idx = pick_random_vid(CARDIO)
-        print(f"{video} video {idx + 1}")
-
-    if arg == "strength":
-        video, idx = pick_random_vid(STRENGTH)
-        print(f"{video} video {idx + 1}")
-
-else:
-    video, idx = pick_random_vid({**STRENGTH, **CARDIO})
-    print(f"{video} video {idx + 1}")
+if __name__ == "__main__":
+    app()
